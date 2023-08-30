9ee1386395ab69847a40bbe8d75aaa9c5b0e0b62
rmc9311 <77700960+rmc9311@users.noreply.github.com>
2023-08-30 12:34:46 -0700

    Update SslServerApplication.java

--- SslServerApplication.java
+++ SslServerApplication.java
@@ -15,7 +15,7 @@ public class SslServerApplication {
 		SpringApplication.run(SslServerApplication.class, args);
 	}
 	
-//FIXME: Add route to enable check sum return of static data example:  String data = "Hello World Check Sum!";
+
 class ServerController{
 	@RequestMapping("/hash")
 	public String myHash()
